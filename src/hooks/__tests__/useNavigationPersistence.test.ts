a10e78258052aec73749ab0dc7a440cd2656f63c
Jack Haas <johnehaas30@gmail.com>
2025-06-13 14:55:01 -0400

    Fix ESLint warning: remove unused waitFor import

--- src/hooks/__tests__/useNavigationPersistence.test.ts
+++ src/hooks/__tests__/useNavigationPersistence.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act, waitFor } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 import { useRouter, usePathname } from 'next/navigation';
 import { useNavigationPersistence } from '../useNavigationPersistence';
 import { useNavigationStore } from '@/state/navigationStore';
