af15a3bf01baa76570001991c9c7a506fbfb26fc
Jack Haas <johnehaas30@gmail.com>
2026-01-04 22:32:13 -0500

    fix: add missing spaces in string concatenation
    Addresses GitHub Advanced Security code scanning alerts for missing spaces in error messages.

--- .dependency-cruiser.cjs
+++ .dependency-cruiser.cjs
@@ -212,7 +212,7 @@ module.exports = {
       comment:
         "This module depends on an npm package from the 'devDependencies' section of your " +
         'package.json. It looks like something that ships to production, though. To prevent problems ' +
-        "with npm packages that aren't there on production declare it (only!) in the 'dependencies'" +
+        "with npm packages that aren't there on production declare it (only!) in the 'dependencies' " +
         'section of your package.json. If this module is development only - add it to the ' +
         'from.pathNot re of the not-to-dev-dep rule in the dependency-cruiser configuration',
       from: {
@@ -239,7 +239,7 @@ module.exports = {
       comment:
         "This module depends on an npm package that is declared as an optional dependency " +
         "in your package.json. As this makes sense in limited situations only, it's flagged here. " +
-        "If you use an optional dependency here by design - add an exception to your" +
+        "If you use an optional dependency here by design - add an exception to your " +
         "dependency-cruiser configuration.",
       from: {},
       to: {
